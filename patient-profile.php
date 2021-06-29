19616aead292b9d5222787b65dc8fb8c6b6d1943
Moamen236 <Moamen236@users.noreply.github.com>
2021-06-29 16:56:42 +0200

    new

--- patient-profile.php
+++ patient-profile.php
@@ -93,7 +93,7 @@ $to_do_list = $to_do->selectWhere("*", "patient_id = $patient_id");
                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                                             aria-label="Close"></button>
                                     </div>
-                                    <form action="handle/add-to-do.php" id="form" method="post">
+                                    <form action="handle/add-to-do.php" method="post">
                                         <div class="modal-body">
                                             <div class="mb-3">
                                                 <input type="text" name="title" id="title" class="form-control"
