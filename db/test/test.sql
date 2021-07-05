6ea828082d4c9ef3cd9a0aff98187e0f67b560e5
Moamen236 <Moamen236@users.noreply.github.com>
2021-07-05 02:34:50 +0200

    new

--- db/test/test.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `short_term` (`id`, `short_term_description`, `plan_id`) VALUES
-(101, 'Aut voluptas iure unde eum aut ad aspernatur. Reiciendis dolore maxime hic ipsam aperiam. Est nesciunt deserunt et consequatur est quaerat id. Ducimus non non ducimus molestiae. Dolor dolorum est ipsum ullam.', 1),
-(102, 'Est similique et deserunt. Non voluptatum unde quae quia debitis animi doloribus. Ut a earum optio libero quibusdam doloremque voluptatem.', 1),
-(103, 'Et eum totam officiis vel molestiae alias. Praesentium omnis nulla id expedita corporis molestiae dolorum non. Et voluptates qui consequuntur quia.', 1),
-(104, 'Est et distinctio pariatur sunt aliquam deleniti blanditiis. Possimus quisquam cumque quia at. Aut dicta minima voluptatem alias velit. Qui est iure quo ex dolores praesentium.', 1),
-(105, 'Harum aperiam maxime id debitis consectetur. Aperiam sit harum adipisci illo nam enim ut id. Distinctio quia repudiandae fugit nisi quo.', 1),
-(106, 'Eaque culpa ullam ut voluptates saepe laudantium. Non eum et harum mollitia voluptatibus nihil incidunt. Est dolore quo nihil est quo qui.', 1),
-(107, 'Qui repudiandae nulla vero dolore. Dolor vitae nesciunt est voluptatem impedit. Vitae dolor maxime non repudiandae. Perspiciatis ipsam ut rerum voluptatum quasi similique. Doloremque eos distinctio corrupti minus quidem.', 1),
-(108, 'Ratione dolores officia repudiandae et quisquam tenetur laboriosam. Est beatae nemo neque sed. Porro magnam unde odit nulla rem dicta voluptas ducimus. Recusandae ullam dignissimos delectus incidunt.', 1);
